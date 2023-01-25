61137a76c36aaed41bda3314b24466edb029a612
highfreshness <meteo0718@gmail.com>
2023-01-25 22:09:00 +0900

    추론 서버 컨테이너 추가

--- model/app/main.py
+++ model/app/main.py
@@ -11,15 +11,14 @@ from app.get_text import ocr_api
 def run(image):
 
     # load YOLOv5 with custom weight
-    model = torch.hub.load('ultralytics/yolov5', 'custom', path='YOLOv5 Weight/custom_221117.pt')
-
+    model = torch.hub.load('ultralytics/yolov5', 'custom', path='./yolov5_weight/custom_221117.pt')
     nob, boxes = inference(model, image) # nob: num of boxes # 유통기한 부분만 크롭
     # box_images = [x[1] for x in boxes]
-
+    texts = []
     for i in range(nob): # 각 박스마다 전처리 후 OCR 하여 texts에 추가
         s, img = boxes[i]
-        texts = []
-
+        # texts = []
+        
         if s: # bounding box 찾은 경우
             pre_img = image_preprocessing(img) # 글자 잘 인식하도록 전처리
             text = ocr_api(pre_img) # 이미지에서 텍스트 추출
@@ -27,6 +26,7 @@ def run(image):
 
         else: # bounding box 찾지 못한 경우
             print("인식하지 못했습니다")
+    
 
     return texts # [['box1', 'ocr', '결과'], ['box2', ' ocr', '결과'], ...]
 
@@ -38,5 +38,4 @@ app = FastAPI()
 def predict_ExpDate(file: bytes = File()):
     decoded = cv2.imdecode(np.frombuffer(file, np.uint8), cv2.IMREAD_COLOR)
     result = run(decoded) # box 개수만큼의 ocr 결과 리스트
-
     return {'exp_date': result}
